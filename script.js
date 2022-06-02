51db39f06cb2eac56da82bbabcd83ba29cae7f23
Vitor <102909348+vitorbertelli@users.noreply.github.com>
2022-06-02 19:27:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function ativarBotao(){
+  const varA = document.querySelector('#varA').value
+  const varB = document.querySelector('#varB').value
+  const varC = document.querySelector('#varC').value
+  if (varA && varB && varC){
+    document.querySelector('#botao').disabled = false
+    return
+  }
+  document.querySelector('#botao').disabled = true
+}
+function calcular(){
+  var A = document.getElementById("varA")
+  var B = document.getElementById("varB")
+  var C = document.getElementById("varC")
+  if(A.value == 0 || (B.value == 0 && C.value == 0)){
+    resultado.innerHTML = "Não é uma equação do 2° Grau."
+  }else{
+    var delta = B.value ** 2 - 4 * A.value * C.value
+    if(delta < 0){
+      resultado.innerHTML = "Não tem raízes no conjunto dos números reais."
+    }else if(delta == 0){
+      var r1 = - B.value / (2 * A.value)
+      resultado.innerHTML = "A equação do 2° Grau tem apenas uma raíz. x' = " + r1
+    }else{
+      var r1 = (- B.value + Math.sqrt(delta)) / (2 * A.value)
+      var r2 = (- B.value - Math.sqrt(delta)) / (2 * A.value)
+      resultado.innerHTML = "As raízes da equação são x' = " + r1 + " e x'' = " + r2
+    }
+  }
+}
+document.addEventListener("keypress", function(e){
+  if(e.key === 'Enter'){
+    var btn = document.querySelector("#botao");
+    btn.click();
+  }
+})
